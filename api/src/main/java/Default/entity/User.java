0c144548d736f6d92042228c124088f438925694
Remi <st178262@stud.uni-stuttgart.de>
2024-05-28 00:32:19 +0200

    adjusted User

--- api/src/main/java/Default/entity/User.java
+++ api/src/main/java/Default/entity/User.java
@@ -1,4 +1,22 @@
 package Default.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public User() {
+    }
 }
