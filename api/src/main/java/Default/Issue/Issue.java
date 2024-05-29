21c9f00c5b2196079fc7059213d60170a8b17f43
Remi <st178262@stud.uni-stuttgart.de>
2024-05-29 13:02:15 +0200

    added Issue

--- api/src/main/java/Default/Issue/Issue.java
+++ api/src/main/java/Default/Issue/Issue.java
@@ -1,5 +1,6 @@
 package Default.Issue;
 
+import Default.GithubRepo.GithubRepo;
 import Default.User.User;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
@@ -9,8 +10,25 @@ import java.sql.Date;
 @Entity
 @Table(name = "\"issue\"")
 public class Issue {
+    
+
     @Id
     @JsonProperty("sha")
     private String id;
+    private Date dateOpened;
 
+    private Date Closed;
+    private String State;
+    @OneToOne
+    @JoinColumn(name = "closedBy_id")
+    @JsonProperty("author")
+    private User closedBy;
+    @OneToOne
+    @JoinColumn(name = "openedBy_id")
+    @JsonProperty("author")
+    private User openedBy;
+    @OneToOne
+    @JoinColumn(name = "author_id")
+    @JsonProperty("author")
+    private GithubRepo repository;
 }
