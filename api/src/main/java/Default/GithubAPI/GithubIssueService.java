cf2bf551d948e0d66a02f3c17632594781abdbb5
Remi <st178262@stud.uni-stuttgart.de>
2024-05-29 13:12:55 +0200

    issueService

--- api/src/main/java/Default/GithubAPI/GithubIssueService.java
+++ api/src/main/java/Default/GithubAPI/GithubIssueService.java
@@ -109,13 +109,14 @@ public class GithubIssueService {
      * @return Single Issue with als attributes
      */
     private Mono<Issue> fetchIssueDetails(Issue issue, String owner, String repo) {
+        
         String url = String.format("/repos/%s/%s/issues/%s", owner, repo, issue.getId());
         return webClient.get()
             .uri(url)
             .retrieve()
             .bodyToMono(IssueDetails.class)
             .map(details -> {
-                issue.setAdditions(details.getStats().getAdditions());
+                issue.setDateOpened(details.getIssueInfo().getCreatedAt());
                 issue.setDeletions(details.getStats().getDeletions());
                 issue.setDate(details.getIssueInfo().getAuthor().getDate());
                 issue.setMessage(details.getIssueInfo().getMessage());
@@ -123,113 +124,32 @@ public class GithubIssueService {
             })
             .map(issues -> setIssueMerge(issue));
     }
-
-    /**
-     * Sets the boolean whether the issue is a merge issue
-     * Depends on the message if it contains "Merge"
-     *
-     * @param issue
-     * @return Issue with set merge boolean
-     */
-    private Issue setIssueMerge(Issue issue) {
-        if (issue.getMessage() != null) {
-            issue.setMerge(issue.getMessage().contains("Merge"));
-        }
-        return issue;
-    }
+    
 
     /**
      * Anonym Class for Requesting Issue Details
      * Represents the structure of the response of the GitHub API
      */
     private static class IssueDetails {
-        @JsonProperty("issue")
-        private IssueInfo issueInfo;
 
-        @JsonProperty("stats")
-        private Stats stats;
+        
 
-        public IssueInfo getIssueInfo() {
-            return issueInfo;
+        Date createdAt;
+        Date updatedAt;
+        public Date getCreatedAt() {
+            return createdAt;
         }
 
-        public void setIssueInfo(IssueInfo issueInfo) {
-            this.issueInfo = issueInfo;
+        public void setCreatedAt(Date createdAt) {
+            this.createdAt = createdAt;
         }
 
-        public Stats getStats() {
-            return stats;
+        public Date getUpdatedAt() {
+            return updatedAt;
         }
 
-        public void setStats(Stats stats) {
-            this.stats = stats;
-        }
-
-        private static class IssueInfo {
-            @JsonProperty("author")
-            private Author author;
-
-            @JsonProperty("message")
-            private String message;
-
-            public Author getAuthor() {
-                return author;
-            }
-
-            public void setAuthor(Author author) {
-                this.author = author;
-            }
-
-            public String getMessage() {
-                return message;
-            }
-
-            public void setMessage(String message) {
-                this.message = message;
-            }
-
-            private static class Author {
-                @JsonProperty("date")
-                private Date date;
-
-                public Date getDate() {
-                    return date;
-                }
-
-                public void setDate(Date date) {
-                    this.date = date;
-                }
-            }
-        }
-
-        private static class Stats {
-            private int total;
-            private int additions;
-            private int deletions;
-
-            public int getTotal() {
-                return total;
-            }
-
-            public void setTotal(int total) {
-                this.total = total;
-            }
-
-            public int getAdditions() {
-                return additions;
-            }
-
-            public void setAdditions(int additions) {
-                this.additions = additions;
-            }
-
-            public int getDeletions() {
-                return deletions;
-            }
-
-            public void setDeletions(int deletions) {
-                this.deletions = deletions;
-            }
+        public void setUpdatedAt(Date updatedAt) {
+            this.updatedAt = updatedAt;
         }
     }
 }
