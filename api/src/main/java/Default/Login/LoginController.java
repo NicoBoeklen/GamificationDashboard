9d479b7f39972bfbc92d64ffaefff5f5a12b4253
Remi <st178262@stud.uni-stuttgart.de>
2024-06-01 12:15:34 +0200

    fixed LoginService

--- api/src/main/java/Default/Login/LoginController.java
+++ api/src/main/java/Default/Login/LoginController.java
@@ -7,8 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class LoginController {
     @Autowired
     LoginService loginService;
