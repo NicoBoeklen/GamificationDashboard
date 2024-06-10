76ce5a4ef24cfb3ca9d4aa7a1959f22ebb0dc5ba
lucagroner <luca.groner@uni-hohenheim.de>
2024-06-10 21:00:10 +0200

    dashboard

--- frontend/src/components/DashboardHome.vue
+++ frontend/src/components/DashboardHome.vue
@@ -1,140 +1,119 @@
 <template>
   <Header/>
-  <section class="insight-section">
-    <div class="commit-container">
-      <div class="commit-info">
-        <span class="circle">39</span>
-        <div class="progress-bar-container">
+  <section class="container">
+    <header class="counter-wrapper">
+      <div class="counter-box">
+        <div class="counter-number">39</div>
+        <div class="progress-bar-wrapper">
           <div class="progress-bar"></div>
         </div>
       </div>
-      <article class="commit-article">
-        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a15a58212de51f3f8af173f0f65e334091b691385ab3ae8e623d83ef49bba2bc?apiKey=b979e3b43d464a49a8e4c392f6c7b6d1&" alt="Commitment Image" class="commit-image" />
-        <div class="commit-details">
-          <span class="commit-days">... Days of COMMITment</span>
-        </div>
-      </article>
-    </div>
-    <nav class="insight-navigation">Insights</nav>
+      <div class="commitment-wrapper">
+        <img loading="lazy" src="https://cdn.builder.io/api/v1/image/assets/TEMP/a15a58212de51f3f8af173f0f65e334091b691385ab3ae8e623d83ef49bba2bc?apiKey=b979e3b43d464a49a8e4c392f6c7b6d1&" class="commitment-logo" alt="Commitment Logo" />
+        <p class="commitment-text"> <span class="strong-text">400</span> <span>Days of</span> <span class="bold-text">Commit</span> <span>ment</span> </p>
+      </div>
+    </header>
+    <button class="insights-button">Insights</button>
   </section>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-import Header from "./Header.vue";
-
-export default defineComponent({
-  name: 'InsightsComponent'
-});
-</script>
-
 <style scoped>
-.insight-section {
+.container {
   display: flex;
   justify-content: space-between;
   gap: 1px;
   padding: 12px 28px;
   background-color: var(--1st, #5d5a5a);
 }
-
 @media (max-width: 991px) {
-  .insight-section {
+  .container {
     flex-wrap: wrap;
     padding: 0 20px;
   }
 }
 
-.commit-container {
+.counter-wrapper {
   display: flex;
   gap: 1px;
 }
-
-.commit-info {
+.counter-box {
   display: flex;
   gap: 0;
 }
-
-.circle {
+.counter-number {
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
   width: 37px;
   height: 37px;
   padding: 0 10px;
-  border-radius: 50%;
+  border-radius: 262px;
   background-color: #f5f5f5;
   color: #f5c800;
   font: 700 12px Inter, sans-serif;
+  white-space: nowrap;
 }
 
 @media (max-width: 991px) {
-  .circle {
-    white-space: initial;
+  .counter-number {
+    white-space: normal;
   }
 }
-
-.progress-bar-container {
+.progress-bar-wrapper {
   display: flex;
   flex-direction: column;
   justify-content: center;
   margin-top: 8px;
-  background-color: #fff;
   border-radius: 26px;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background-color: #fff;
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
 }
-
 .progress-bar {
+  width: 21px;
   height: 21px;
-  background-color: #ffdd61;
   border-radius: 26px 0 0 26px;
+  background-color: #ffdd61;
 }
-
-.commit-article {
+.commitment-wrapper {
   display: flex;
-  align-items: center;
+  justify-content: center;
   gap: 8px;
   padding: 0 17px;
   font-size: 12px;
-  color: var(--FrameColor, #fff);
   font-weight: 400;
+  color: var(--FrameColor, #fff);
 }
-
-.commit-image {
+.commitment-logo {
   width: 29px;
   aspect-ratio: 0.75;
   object-fit: cover;
   object-position: center;
 }
-
-.commit-details {
-  display: flex;
-  align-items: center;
+.commitment-text {
+  margin: auto 0;
   font-family: Inter, sans-serif;
 }
-
-.commit-days, .commit-strong {
-  font-weight: bold;
-  color: #fff;
+.strong-text {
+  font-weight: 700;
+  color: rgba(255, 255, 255, 1);
 }
-
-.commit-text {
-  color: #fff;
+.bold-text {
+  font-weight: 800;
+  color: rgba(255, 255, 255, 1);
 }
-
-.insight-navigation {
-  display: flex;
-  align-items: center;
-  justify-content: center;
+.insights-button {
   padding: 6px 18px;
+  border-radius: 11px;
   background-color: var(--Button, #6f6b6b);
   color: #fff;
   font: 400 24px Inter, sans-serif;
-  border-radius: 11px;
+  white-space: nowrap;
 }
-
 @media (max-width: 991px) {
-  .insight-navigation {
-    white-space: initial;
+  .insights-button {
+    white-space: normal;
   }
 }
 </style>
+<script setup lang="ts">
+import Header from "./Header.vue";
+</script>
