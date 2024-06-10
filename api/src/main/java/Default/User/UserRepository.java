5e9ec304e60da8615f7b110344b30350d98449b2
Remi <st178262@stud.uni-stuttgart.de>
2024-06-10 19:41:15 +0200

    added AvatarMapping

--- api/src/main/java/Default/User/UserRepository.java
+++ api/src/main/java/Default/User/UserRepository.java
@@ -1,9 +1,13 @@
 package Default.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for Users identified by ID (long)
  */
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u.avatarUrl FROM User u WHERE u.name = :username")
+    String getUserAvatar(@Param("username") String username);
 }
