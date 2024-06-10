08e86c768688ee5126ac1ced86bfb8e364bdc52a
Remi <st178262@stud.uni-stuttgart.de>
2024-06-10 18:45:23 +0200

    added Cors to backend

--- api/src/main/java/Default/Application.java
+++ api/src/main/java/Default/Application.java
@@ -1,7 +1,11 @@
 package Default;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Application {
@@ -9,4 +13,15 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@Nonnull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedMethods("OPTIONS", "HEAD", "GET", "PUT", "POST", "DELETE")
+                    .allowedOrigins("http://localhost:3000");
+            }
+        };
 }
