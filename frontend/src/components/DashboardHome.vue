8aa8b5d5d63ab13252480c82ae58b1aa38bd51d2
lucagroner <luca.groner@uni-hohenheim.de>
2024-06-12 18:33:47 +0200

    New Element

--- frontend/src/components/DashboardHome.vue
+++ frontend/src/components/DashboardHome.vue
@@ -49,7 +49,49 @@
               <br />
               Updated at: 25.05.2024
             </v-card-text>
-            <v-card-actions class="project-stats"> </v-card-actions>
+            <v-card-actions class="project-stats">
+              <v-carousel hide-delimiters height="300" cycle interval="15000">
+                <v-carousel-item>
+                  <v-container class="fill-height" fluid>
+                    <v-row align="center" justify="center">
+                      <v-col class="text-center">
+                        <div>
+                          <h2>Erstes Item</h2>
+                          <p>Beschreibung des ersten Items</p>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-carousel-item>
+
+                <v-carousel-item>
+                  <v-container class="fill-height" fluid>
+                    <v-row align="center" justify="center">
+                      <v-col class="text-center">
+                        <div>
+                          <h2>Zweites Item</h2>
+                          <p>Beschreibung des zweiten Items</p>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-carousel-item>
+
+                <v-carousel-item>
+                  <v-container class="fill-height" fluid>
+                    <v-row align="center" justify="center">
+                      <v-col class="text-center">
+                        <div>
+                          <h2>Drittes Item</h2>
+                          <p>Beschreibung des dritten Items</p>
+                        </div>
+                      </v-col>
+                    </v-row>
+                  </v-container>
+                </v-carousel-item>
+              </v-carousel>
+
+            </v-card-actions>
           </v-card>
 
 
