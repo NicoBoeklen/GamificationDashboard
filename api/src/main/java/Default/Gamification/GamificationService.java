af641ca3c8450e94450fa331f7f355eb941a160e
NicoBoeklen <Nico3Boe@gmail.com>
2024-06-14 13:14:07 +0200

    Error falls null Werte gefixt

--- api/src/main/java/Default/Gamification/GamificationService.java
+++ api/src/main/java/Default/Gamification/GamificationService.java
@@ -34,7 +34,7 @@ public class GamificationService {
         Skill skillsUser = new Skill();
 
         //Commit-Value
-        if (commitService.getMaxCommitsSingleUser(repoId) != 0) {
+        if (commitService.getMaxCommitsSingleUser(repoId) != null && commitService.getMaxCommitsSingleUser(repoId) != 0) {
             System.out.println("Commits user: " + commitService.getCommitCount(userId, repoId) + "Commits MAX: " + commitService.getMaxCommitsSingleUser(repoId));
             Double commitValue = ((double)commitService.getCommitCount(userId, repoId) / (double)commitService.getMaxCommitsSingleUser(repoId)) * 10.0;
             skillsUser.setCommitValueUser(commitValue);
@@ -44,7 +44,7 @@ public class GamificationService {
         }
 
         //Productivity-Value
-        if (commitService.getMaxProductivitySingleUser(repoId) != 0) {
+        if (commitService.getMaxProductivitySingleUser(repoId) != null && commitService.getMaxProductivitySingleUser(repoId) != 0) {
             System.out.println("Prod user: " + commitService.getAverageUserProductivity(userId, repoId) + "Prod MAX: " + commitService.getMaxProductivitySingleUser(repoId));
             Double prodValue = (commitService.getAverageUserProductivity(userId, repoId) / commitService.getMaxProductivitySingleUser(repoId)) * 10.0;
             skillsUser.setProductivityValueUser(prodValue);
@@ -54,7 +54,7 @@ public class GamificationService {
         }
         
         //Issue-Value
-        if (issueService.getMaxFixedIssuesSingleUser(repoId) != 0) {
+        if (issueService.getMaxFixedIssuesSingleUser(repoId) != null && issueService.getMaxFixedIssuesSingleUser(repoId) != 0) {
             System.out.println("Issue user: " + issueService.getTotalClosedIssuesUser(userId, repoId) + "Issue MAX: " + issueService.getMaxFixedIssuesSingleUser(repoId));
             Double issueValue = ((double)issueService.getTotalClosedIssuesUser(userId, repoId) / (double)issueService.getMaxFixedIssuesSingleUser(repoId)) * 10.0;
             skillsUser.setFixedIssuesValueUser(issueValue);
@@ -63,7 +63,7 @@ public class GamificationService {
             System.out.println("Keine Issues gefixt");
         }
         //Review-Value
-        if (pullRequestService.getMaxReviewsSingleUser(repoId) != 0) {
+        if (pullRequestService.getMaxReviewsSingleUser(repoId) != null && pullRequestService.getMaxReviewsSingleUser(repoId) != 0) {
             System.out.println("Review user: " + pullRequestService.getNumberReviews(userId, repoId) + "Review MAX: " + pullRequestService.getMaxReviewsSingleUser(repoId));
             Double reviewValue = ((double)pullRequestService.getNumberReviews(userId, repoId) / (double)pullRequestService.getMaxReviewsSingleUser(repoId)) * 10.0;
             skillsUser.setReviewValueUser(reviewValue);
