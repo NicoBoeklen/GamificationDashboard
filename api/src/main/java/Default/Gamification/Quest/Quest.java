2c0a3a393bec5c0add2fb842f938a95b1b7dc431
NicoBoeklen <Nico3Boe@gmail.com>
2024-06-15 17:41:34 +0200

    Quest fix

--- api/src/main/java/Default/Gamification/Quest/Quest.java
+++ api/src/main/java/Default/Gamification/Quest/Quest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "\"quest\"")
 public class Quest {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -15,20 +16,20 @@ public class Quest {
     private String description;
     private int xp;
     private String type;
+
     private int condition;
-    private LocalDateTime day;
+    private LocalDateTime tag;
 
     public Quest() {
     }
 
-    public Quest(Long id, String name, String description, int xp, String type, int condition, LocalDateTime day) {
-        this.id = id;
+    public Quest(String name, String description, int xp, String type, int condition, LocalDateTime tag) {
         this.name = name;
         this.description = description;
         this.xp = xp;
         this.type = type;
         this.condition = condition;
-        this.day = day;
+        this.tag = tag;
     }
 
     public Long getId() {
@@ -80,10 +81,10 @@ public class Quest {
     }
 
     public LocalDateTime getDay() {
-        return day;
+        return tag;
     }
 
     public void setDay(LocalDateTime day) {
-        this.day = day;
+        this.tag = day;
     }
 }
--- api/src/main/java/Default/Gamification/Quest/UserQuestRepository.java
+++ api/src/main/java/Default/Gamification/Quest/UserQuestRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface UserQuestRepository extends JpaRepository<UserQuest, Long> {
+    
     @Query("SELECT CASE WHEN COUNT(ua) > 0 THEN TRUE ELSE FALSE END FROM UserQuest ua WHERE ua.user = :user AND ua.quest = :quest")
     boolean existsByUserAndQuest(@Param("user") User user, @Param("quest") Quest quest);
 }
