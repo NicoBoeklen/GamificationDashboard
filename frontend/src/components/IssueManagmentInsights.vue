0c7dabcc04540b58bd8143715daa079eeb6941db
Remi <st178262@stud.uni-stuttgart.de>
2024-06-16 14:23:12 +0200

    added issues

--- frontend/src/components/IssueManagmentInsights.vue
+++ frontend/src/components/IssueManagmentInsights.vue
@@ -21,6 +21,9 @@
 
     </v-app-bar>
   </v-container>
+  <v-container>
+
+  </v-container>
 </template>
 <script setup lang="ts">
 import HeaderInsights from "./HeaderInsights.vue";
@@ -30,6 +33,12 @@ import {
   redirectDeployment,
   redirectIssueManagment
 } from "../objects/directions";
+import {onMounted, ref, type Ref} from "vue";
+import {fetchIssues} from "../objects/issues";
+onMounted(async () => {
+  const issues = await fetchIssues();
+  console.log("Amount Total Closed Issues" + issues.amountFixedIssuesTeam);
+});
 </script>
 <style>
 .button{
