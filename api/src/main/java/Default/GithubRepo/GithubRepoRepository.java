89af8d06973ec735a6467df6b9f051e86dd0e9b3
Remi <st178262@stud.uni-stuttgart.de>
2024-06-17 14:20:32 +0200

    added Repository API

--- api/src/main/java/Default/GithubRepo/GithubRepoRepository.java
+++ api/src/main/java/Default/GithubRepo/GithubRepoRepository.java
@@ -1,10 +1,14 @@
 package Default.GithubRepo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for GitHub-Repository identified by ID (long)
  */
 public interface GithubRepoRepository extends JpaRepository<GithubRepo, Long> {
+    @Query("SELECT r FROM GithubRepo r WHERE r.owner.repoId=:repoId AND r.owner.userId=:userId")
+    GithubRepo findByRepoIdUserId(@Param("userId") Long userId, @Param("repoId") Long repoId);
 }
 
