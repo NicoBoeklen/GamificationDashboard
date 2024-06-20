affca43822fa736afc7716e48fc4632b2f9484a6
NicoBoeklen <Nico3Boe@gmail.com>
2024-06-20 10:57:10 +0200

    Dashboard Home angepasst

--- frontend/src/components/DashboardHome.vue
+++ frontend/src/components/DashboardHome.vue
@@ -1,6 +1,6 @@
 <template>
     <Header/>
-    <v-container class="container">
+    <v-container fluid class="container">
       <v-row>
         <v-col cols="12" md="4">
           <div class="d-flex align-center">
@@ -32,7 +32,7 @@
     </v-container>
 
 
-    <v-container class="main-container">
+    <v-container fluid class="main-container">
       <v-row>
         <v-col cols="12" md="4" class="content-section" >
           <v-card class="project-info">
@@ -42,14 +42,14 @@
                 <p class="project-subtitle">{{ repoName }}</p>
               </div>
             </v-card-title>
-            <v-card-text class="project-description">
+            <v-card-text style="margin-bottom: 0; padding-bottom: 0" class="project-description">
               Description: {{ repository.description }}
               <br />
               Created at: {{ repository.created_at }}
               <br />
               Updated at: {{ repository.updated_at }}
             </v-card-text>
-              <v-carousel hide-delimiters height="300" cycle interval="15000">
+              <v-carousel hide-delimiters height="10em" cycle interval="15000">
                 <v-carousel-item>
                   <v-container class="fill-height" fluid>
                     <v-row align="center" justify="center">
--- frontend/src/components/ManagementView.vue
+++ frontend/src/components/ManagementView.vue
@@ -11,14 +11,14 @@
             <p class="project-subtitle">{{ repoName }}</p>
           </div>
         </v-card-title>
-        <v-card-text class="project-description">
+        <v-card-text style="margin-bottom: 0; padding-bottom: 0" class="project-description">
           Description: {{ repository.description }}
           <br />
           Created at: {{ repository.created_at }}
           <br />
           Updated at: {{ repository.updated_at }}
         </v-card-text>
-        <v-carousel hide-delimiters height="300" cycle interval="15000">
+        <v-carousel hide-delimiters height="10em" cycle interval="15000">
           <v-carousel-item>
             <v-container class="fill-height" fluid>
               <v-row align="center" justify="center">
