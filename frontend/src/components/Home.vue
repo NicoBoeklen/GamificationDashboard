1676a452eaeb297bbcf5deee5ca173e988c7d862
NicoBoeklen <Nico3Boe@gmail.com>
2024-06-20 15:42:35 +0200

    Login Style

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,29 +1,21 @@
 <template>
-  <v-container class="fill-height">
-    <v-responsive
-      class="align-center fill-height mx-auto"
-      max-width="900"
-    >
-      <v-img
-        class="mb-4"
-        height="200"
-        src="@/assets/LogoDashboard.png"
-      />
+  <v-container class="d-flex align-center justify-center fill-height" style="max-width: 70%; margin: 0 auto; padding: 0;">
+    <v-responsive class="text-center" style="margin:0; padding: 0;">
+      <v-img class="mb-4 mx-auto" height="200" src="@/assets/LogoDashboard.png" />
 
       <div class="text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-
         <h1 class="text-h2 font-weight-bold">GameDash</h1>
       </div>
 
       <div class="py-4" />
 
       <v-row align="center" justify="center">
-        <v-col cols="12" sm="10" md="6" lg="4">
-          <v-card class="pa-5">
+        <v-col cols="12" sm="10" md="8" lg="6" xl="4">
+          <v-card class="pa-5 mb-4" :class="{ highlighted: isCurrentUser }">
             <v-card-title class="headline">Login</v-card-title>
-            <v-card-text>
-              <v-form  ref="form">
+            <v-card-text style="margin:0; padding: 0;">
+              <v-form ref="form">
                 <v-text-field
                   label="Username"
                   :rules="[rules.required]"
@@ -60,8 +52,7 @@
                   dense
                   class="mb-4"
                 ></v-text-field>
-                <v-btn color="primary" type="button" block  class="mt-4" @click="login()">Login</v-btn>
-
+                <v-btn color="primary" type="button" block class="mt-4" @click="login()">Login</v-btn>
               </v-form>
             </v-card-text>
           </v-card>
@@ -71,22 +62,30 @@
   </v-container>
 </template>
 
-
 <script setup lang="ts">
-import {toLogin} from "../objects/login";
-import {login} from "../objects/login";
+import { toLogin } from "../objects/login";
+import { login } from "../objects/login";
 const rules = {
   required: (value: string) => !!value || 'Required!'
 };
-
 </script>
+
 <style scoped>
 .fill-height {
-  min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  min-height: 20%;
+}
 
+.highlighted {
+  background-color: #f0f8ff; /* Set a different background color for highlighted cards */
+}
+
+.text-center {
+  text-align: center;
+}
+
+.mx-auto {
+  margin-left: auto;
+  margin-right: auto;
 }
 
 v-card {
@@ -99,10 +98,6 @@ v-card-title {
   font-size: 24px;
   font-weight: bold;
   color: #3f51b5;
-}
-
-v-text-field {
-  margin-bottom: 16px;
 }
 
 v-btn {
